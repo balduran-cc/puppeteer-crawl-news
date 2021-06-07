5f20be027a00c30a7fee2c8a4adc42c729dc7657
balduran <balduran@gmail.com>
2021-06-07 16:12:43 +0800

    install lib

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 async function run() {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
 
   await page.goto('https://us.yahoo.com/');
